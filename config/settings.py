772c85d8f004dde54640f18279e865d691e34f24
grbeno <grbeno@gmail.com>
2022-02-08 16:09:01 +0100

    test-heroku-1

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  True #env.bool("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'user-admin-template.herokuapp.com' ] #  '.herokuapp.com', You can add more production url here!
 
